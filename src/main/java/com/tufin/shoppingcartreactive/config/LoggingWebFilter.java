e6ebcd50903954aa1b2557d5a0b40a3290567473
michal <michael6cohen@gmail.com>
2025-10-16 12:02:27 +0300

    revert

--- src/main/java/com/tufin/shoppingcartreactive/config/LoggingWebFilter.java
+++ src/main/java/com/tufin/shoppingcartreactive/config/LoggingWebFilter.java
@@ -1,6 +1,5 @@
 package com.tufin.shoppingcartreactive.config;
 
-import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
@@ -11,12 +10,12 @@ import org.springframework.web.server.WebFilter;
 import org.springframework.web.server.WebFilterChain;
 import reactor.core.publisher.Mono;
 
-import java.util.stream.Collectors;
 
-@Slf4j
 @Configuration
 @Order(-1)
 public class LoggingWebFilter implements WebFilter {
+
+    private static final Logger log = LoggerFactory.getLogger(LoggingWebFilter.class);
 
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
@@ -25,21 +24,7 @@ public class LoggingWebFilter implements WebFilter {
         String path = req.getURI().getPath();
         String method = req.getMethod().name();
 
-        return exchange.getRequest().getBody()
-                .collectList()
-                .flatMap(dataBuffers -> {
-                    if (log.isDebugEnabled() && "POST".equalsIgnoreCase(method) && path.equals("/graphql")) {
-                        String body = dataBuffers.stream()
-                                .map(buffer -> {
-                                    byte[] bytes = new byte[buffer.readableByteCount()];
-                                    buffer.read(bytes);
-                                    return new String(bytes);
-                                })
-                                .collect(Collectors.joining());
-                        log.debug("GraphQL request body: {}", body);
-                    }
-                    return chain.filter(exchange);
-                })
+        return chain.filter(exchange)
                 .doOnSubscribe(s -> log.info("Incoming request: {} {}", method, path))
                 .doOnSuccess(v -> {
                     long took = System.currentTimeMillis() - started;
